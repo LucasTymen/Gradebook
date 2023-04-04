e07fe8252d425865cf37fb66e6749869be145a9b
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-04 13:17:38 +0200

    Gradebook_5

--- script.py
+++ script.py
@@ -1,3 +1,16 @@
 last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
 
 # Your code below:
+# Checkpoint 1 :
+subjects = ["physics", "calculus", "poetry", "history"]
+
+# Checkpoint 2 :
+grades = [98, 97, 85, 88]
+
+# Checkpoint 3 :
+gradebook = [["physics",98],["calculus",97],["poetry",85],["history",88]]
+
+# Checkpoint 4 :
+print(gradebook)
+
+# Checkpoint 5 :
